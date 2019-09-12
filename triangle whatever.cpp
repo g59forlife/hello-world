6ecc8abfde043721dacd05d92261ce1f393fd2ff
g59forlife <55246875+g59forlife@users.noreply.github.com>
2019-09-12 21:53:32 +0700

    Create triangle whatever.cpp

---
+++ triangle whatever.cpp
@@ -0,0 +1,54 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	// получаем идентификатор окна
+	HWND hwnd = GetConsoleWindow();
+	// получаем контекст отображения
+	HDC hdc = GetDC(hwnd);
+	RECT rt;
+	char buf[100];
+	// устанавливаем цвет фона
+	SetBkColor(hdc, RGB(0, 0, 0));
+	// устанавливаем цвет текста
+	SetTextColor(hdc, RGB(255, 0, 0));
+	// создаем красное перо
+	HPEN hRedPen = CreatePen(PS_SOLID, 5, RGB(255, 0, 0));
+	// и выбираем его в контекст отображения,
+	// сохраняя предыдущее перо
+	HPEN hOldPen = SelectPen(hdc, hRedPen);
+	// создаем зеленую кисть
+	HBRUSH hGreenBrush = CreateSolidBrush(RGB(0, 255, 0));
+		// и выбираем ее в контекст отображения,
+		// сохраняя предыдущую кисть
+		HBRUSH hOldBrush = SelectBrush(hdc, hGreenBrush);
+	// выводим строку стандартными средствами
+	printf_s("Graphics in Console Window.");
+	do {
+		// получаем размер окна
+		GetClientRect(hwnd, &rt);
+		// формируем выводимую строку
+		sprintf_s(buf, "Размер окна %d на %d пикселей",
+			rt.right, rt.bottom);
+		// выводим строку графическими средствами
+		TextOutA(hdc, 10, 10, buf, strlen(buf));
+		// рисуем закрашенный эллипс
+		POINT ppt[4] = { {100,100}, {200,200}, {100,300}, {100,100} };
+		Polyline(hdc, ppt, 4);
+	} while (_getch() != 27); // при нажатии любой клавиши
+	// (кроме Esc) перерисовываем изображение,
+	// изображение изменится, если изменились размеры окна,
+	// нажатие Esc – выход
+	// выбираем в контекст отображения предыдущее перо
+	SelectPen(hdc, hOldPen);
+	// выбираем в контекст отображения предыдущую кисть
+	SelectBrush(hdc, hOldBrush);
+	// удаляем красное перо
+	DeletePen(hRedPen);
+	// удаляем зеленую кисть
+	DeleteBrush(hGreenBrush);
+	// освобождаем контекст отображения
+	ReleaseDC(hwnd, hdc);
+}
